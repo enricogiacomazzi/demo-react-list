c16ee443277a0da4ce4000da914ca43e3f152366
Enrico <enrico@giacomazzi.cc>
2022-01-20 11:24:06 +0100

    with bug

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,65 +1,44 @@
-import React, {CSSProperties, useState} from 'react';
-import classNames from 'classnames';
+import React from 'react';
+import {useForm} from 'react-hook-form';
 
 const MyForm: React.FC = () => {
 
-    const formInitialState = {
-        firstname: '',
-        lastname: '',
-        email: '',
-        password: '',
-    };
+    const {register, formState: {isValid, errors}, handleSubmit, watch} = useForm();
 
-    const [form, setForm] = useState(formInitialState);
-    const [dirty, setDirty] = useState<boolean>(false);
+    // const formInitialState = {
+    //     firstname: '',
+    //     lastname: '',
+    //     email: '',
+    //     password: '',
+    // };
 
-    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log('change', e.currentTarget.value);
-        setForm({
-            ...form,
-            [e.currentTarget.name]: e.currentTarget.value
-        });
-        setDirty(true);
-    }
-
-    // const setField = (field: string, e: React.ChangeEvent<HTMLInputElement>) => {
-    //     setForm({
-    //         ...form,
-    //         [field]: e.currentTarget.value
-    //     });
+    // const validationCss = (valid: boolean): string => {
+    //     return classNames(
+    //         'form-control',
+    //         {'is-invalid': !valid && dirty},
+    //         {'is-valid': valid && dirty}
+    //     );
     // }
 
-    const firstnameValid = form.firstname !== '';
-    const lastnameValid = form.lastname.length > 3;
-    const formValid = [firstnameValid, lastnameValid].every(x => x);
-
-
-    const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if(!formValid) {
-            console.log('save', form);
-            // e.currentTarget.reset();
-            setForm(formInitialState);
-        }
+    const submit = (data: any) => {
+        console.log('save', data);
+        // e.currentTarget.reset();
+        // setForm(formInitialState);
     }
 
-    const validationCss = (valid: boolean): string => {
-        return classNames(
-            'form-control',
-            {'is-invalid': !valid && dirty},
-            {'is-valid': valid && dirty}
-        );
-    }
+    console.log(isValid, errors);
+
+    console.log(watch("lastname"));
 
     return (
-        <form onSubmit={onSubmitHandler}>
-            <input name="firstname" value={form.firstname} onChange={onChangeHandler}
-                   type="text" placeholder="firstname" className={validationCss(firstnameValid)}/>
-            <input name="lastname" value={form.lastname} onChange={onChangeHandler}
-                   type="text" placeholder="lastname" className={validationCss(lastnameValid)}/>
-            <input name="email" value={form.email} onChange={onChangeHandler}  type="text" placeholder="email" className="form-control"/>
-            <input name="password" value={form.password} onChange={onChangeHandler}  type="password" placeholder="password" className="form-control"/>
-            <input type="submit" value="salva" className="btn btn-outline-primary" disabled={!formValid} />
+        <form onSubmit={handleSubmit(submit)}>
+            <input {...register('firstname', {required: true})} className="form-control"/>
+            {errors.firstname && "Last name is required"}
+            <input {...register('lastname', {required: true, minLength: 4})} className="form-control"/>
+            <input {...register('email')}  className="form-control"/>
+            <input type="password" {...register('password')}  className="form-control"/>
+            <input type="submit" value="salva" className="btn btn-outline-primary"/>
+            <pre>{isValid ? "valid" : 'invalid'}</pre>
         </form>
     )
 }
